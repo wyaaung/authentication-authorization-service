d544e419cba0124a3d5f29d01be3b7dc63bd3233
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-08-15 02:00:02 +0100

    First Setup for Tests

---
+++ src/test/java/com/wyaaung/rbac/repository/RepositoryTestHelper.java
@@ -0,0 +1,15 @@
+package com.wyaaung.rbac.repository;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import javax.sql.DataSource;
+
+public class RepositoryTestHelper {
+  private final static String DATABASE_SETUP_SQL_PATH = "database-setup.sql";
+
+  public static void resetDatabase(DataSource dataSource) {
+    ScriptUtils.executeSqlScript(DataSourceUtils.getConnection(dataSource), new ClassPathResource(DATABASE_SETUP_SQL_PATH));
+  }
+}
