e9d3d50ac00ed2d6571f392bd86808ec69e2c670
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-08-17 12:37:39 +0100

    Initial Setting up with JWT Authentication

---
+++ src/main/java/com/wyaaung/rbac/configuration/security/AuthConfiguration.java
@@ -0,0 +1,45 @@
+package com.wyaaung.rbac.configuration.security;
+
+import com.wyaaung.rbac.exception.UserNotFoundException;
+import com.wyaaung.rbac.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthConfiguration {
+  private final UserRepository repository;
+
+  public AuthConfiguration(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return username -> repository.getUser(username).orElseThrow(() -> new UserNotFoundException("User not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
---
+++ src/main/java/com/wyaaung/rbac/configuration/security/JwtAuthenticationFilter.java
@@ -0,0 +1,80 @@
+package com.wyaaung.rbac.configuration.security;
+
+import com.wyaaung.rbac.repository.TokenRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Instant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
+  private final TokenRepository tokenRepository;
+
+  @Value("${jwt.header.string}")
+  public String HEADER_STRING;
+
+  @Value("${jwt.token.prefix}")
+  public String TOKEN_PREFIX;
+
+  public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService, TokenRepository tokenRepository) {
+    this.jwtService = jwtService;
+    this.userDetailsService = userDetailsService;
+    this.tokenRepository = tokenRepository;
+  }
+
+  @Override
+  protected void doFilterInternal(
+    HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    if (request.getServletPath().contains("/api/v1/auth")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    final String authHeader = request.getHeader("Authorization");
+    final String jwtToken;
+    final String username;
+
+
+    if (authHeader == null || !authHeader.startsWith(TOKEN_PREFIX)) {
+      LOGGER.warn("Bearer AccessToken Not Found");
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    jwtToken = authHeader.substring(7);
+    username = jwtService.extractUsername(jwtToken);
+
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+      boolean isTokenValid =
+        tokenRepository.getToken(jwtToken).map(
+          accessToken -> accessToken.expiresAt().isBefore(Instant.now()) && accessToken.username().equals(username)
+        ).orElse(false);
+
+      if (isTokenValid) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+          userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+      }
+
+    }
+    filterChain.doFilter(request, response);
+  }
+}
