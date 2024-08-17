baa1e13858be5a9f990a0e368b3d3a8725fa46db
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-08-17 12:31:20 +0100

    User Domain mplements UserDetails. Will be later encapsulated to Auth Objects

--- src/main/java/com/wyaaung/rbac/domain/User.java
+++ src/main/java/com/wyaaung/rbac/domain/User.java
@@ -1,4 +1,81 @@
 package com.wyaaung.rbac.domain;
 
-public record User(String username, String fullName, String password, String emailAddress) {
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+  private String username;
+  private String fullName;
+  private String password;
+  private String emailAddress;
+
+  public User(String username, String fullName, String password, String emailAddress) {
+    this.username = username;
+    this.fullName = fullName;
+    this.password = password;
+    this.emailAddress = emailAddress;
+  }
+
+  public User() {
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
