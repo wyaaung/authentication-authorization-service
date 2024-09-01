957f0f3a3c0ba7e21a226b20555caf9c6bb036cb
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-09-01 12:09:37 +0100

    Moved the files for better arrangement

--- src/test/java/com/wyaaung/rbac/RepositoryTestHelper.java
+++ src/test/java/com/wyaaung/rbac/RepositoryTestHelper.java
@@ -1,15 +1,15 @@
-package com.wyaaung.rbac.unit;
+package com.wyaaung.rbac;
 
+import javax.sql.DataSource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
-import javax.sql.DataSource;
-
 public class RepositoryTestHelper {
-  private final static String DATABASE_SETUP_SQL_PATH = "database-setup.sql";
+  private static final String DATABASE_SETUP_SQL_PATH = "database-setup.sql";
 
   public static void resetDatabase(DataSource dataSource) {
-    ScriptUtils.executeSqlScript(DataSourceUtils.getConnection(dataSource), new ClassPathResource(DATABASE_SETUP_SQL_PATH));
+    ScriptUtils.executeSqlScript(
+      DataSourceUtils.getConnection(dataSource), new ClassPathResource(DATABASE_SETUP_SQL_PATH));
   }
 }
